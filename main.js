7f96d4f7bd46824be919981d7f215ed2b87266c2
talantikus <unnmail@gmail.com>
2022-12-08 19:32:53 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
-const keyboard = navigator.keyboard;
-keyboard.getLayoutMap()
-.then((keyboardLayoutMap) => {
-  const upKey = keyboardLayoutMap.get('KeyW');
-  window.alert(`Press ${upKey} to move up.`);
-})
+
