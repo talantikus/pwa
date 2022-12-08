114bb8f8ccbcf22081ff7c6566bc2b8e6fcb109e
talantikus <unnmail@gmail.com>
2022-12-08 16:44:44 +0600

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -4,10 +4,10 @@ self.addEventListener("install", event => {
 	// Kick out the old service worker
 	self.skipWaiting();
 
-	event.waitUntil(
-		caches.open(cacheName).then(cache => {
-			return cache.addAll([
-// "/",
+	//event.waitUntil(
+		//caches.open(cacheName).then(cache => {
+			//return cache.addAll([
+				// "/",
 				// "/icons/android-chrome-36x36.png", // Favicon, Android Chrome M39+ with 0.75 screen density
 				// "/icons/android-chrome-48x48.png", // Favicon, Android Chrome M39+ with 1.0 screen density
 				// "/icons/android-chrome-72x72.png", // Favicon, Android Chrome M39+ with 1.5 screen density
@@ -44,9 +44,9 @@ self.addEventListener("install", event => {
 				// "/icons/safari-pinned-tab.svg", // Favicon, Safari pinned tab
 				// "/icons/share.jpg", // Social media sharing
 				// "/css/style.css", // Main CSS file
-			]);
-		})
-	);
+			//]);
+		//})
+	//);
 });
 
 self.addEventListener("activate", event => {
