dc428f46fae4f7c01c0daf593effb64f9c7691ee
talantikus <unnmail@gmail.com>
2024-09-26 10:37:18 +0600

    rename title and h1 in PHP file

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PWA Full Features Demo</title>
+    <title>PWA</title>
     <link rel="manifest" href="manifest.json">
 </head>
 <body>
-    <h1>PWA Full Features Demo</h1>
+    <h1>PWA</h1>
 
     <!-- Геолокация -->
     <button onclick="getLocation()">Геолокация</button>
